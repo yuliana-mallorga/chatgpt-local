9c9b6ec6fda04ed17b78f81aa26a795577d39825
Yuliana Mallorga <yulianamallorga@gmail.com>
2024-06-15 20:15:03 -0300

    Fix format

--- worker.js
+++ worker.js
@@ -2,6 +2,6 @@ import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm";
 
 const handler = new WebWorkerMLCEngineHandler();
 
-self.onmessage = function(event) {
+self.onmessage = function (event) {
     handler.onmessage(event);
 }
